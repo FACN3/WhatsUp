0ed9fa61e8c042fec25ade9104160e2f76cdfd8f
nicholaspeal <nicholaspeal@gmail.com>
2017-12-13 17:32:00 +0200

    message submit handler and postData Reference #24

--- src/handler.js
+++ src/handler.js
@@ -1,48 +1,46 @@
-require("env2")("config.env");
-const fs = require("fs");
-const path = require("path");
-const queryString = require("querystring");
-const hash = require("./hash");
-const postData = require("./queries/postData");
-const getData = require("./queries/getData");
-const {sign, verify } = require("jsonwebtoken");
-
+require('env2')('config.env');
+const fs = require('fs');
+const path = require('path');
+const queryString = require('querystring');
+const hash = require('./hash');
+const postData = require('./queries/postData');
+const getData = require('./queries/getData');
+const { sign, verify } = require('jsonwebtoken');
 
 const publicHandler = (req, res) => {
   var url = req.url;
-  if (url === "/") {
-    url = "/public/login.html";
-  } else if (url.includes("/signup")) {
-    url = "/public/signUp.html";
+  if (url === '/') {
+    url = '/public/login.html';
+  } else if (url.includes('/signup')) {
+    url = '/public/signUp.html';
+  } else if (url.includes('/chatRoom')) {
+    url = '/public/chatRoom.html';
   }
-  else if (url.includes("/chatRoom")) {
-    url = "/public/chatRoom.html";
-  }
-  var parts = url.split(".")[1];
+  var parts = url.split('.')[1];
   var extensionType = {
-    css: "text/css",
-    html: "text/html",
-    js: "application/javascript",
-    ico: "image/x-icon",
-    json: "application/json"
+    css: 'text/css',
+    html: 'text/html',
+    js: 'application/javascript',
+    ico: 'image/x-icon',
+    json: 'application/json'
   }[parts];
-  fs.readFile(path.join(__dirname, "..", url), (err, data) => {
+  fs.readFile(path.join(__dirname, '..', url), (err, data) => {
     if (err) {
-      res.writeHead(500, { "content-type": "text/html" });
-      return res.end("Internal Server Error");
+      res.writeHead(500, { 'content-type': 'text/html' });
+      return res.end('Internal Server Error');
     }
-    res.writeHead(200, { "content-type": extensionType });
+    res.writeHead(200, { 'content-type': extensionType });
     res.end(data);
   });
 };
 
 //sign up page
 const createUser = (req, res) => {
-  let data = "";
-  req.on("data", chunk => {
+  let data = '';
+  req.on('data', chunk => {
     data += chunk;
   });
-  req.on("end", () => {
+  req.on('end', () => {
     const formSubmittedData = queryString.parse(data);
     hash.hashPassword(formSubmittedData.password, (err, newPassword) => {
       if (err) {
@@ -55,28 +53,27 @@ const createUser = (req, res) => {
         // formSubmittedData.password = newPassword
         postData.createUser(values, (err, data) => {
           if (err) {
-            res.writeHead(500, { "content-type": "text/html" });
+            res.writeHead(500, { 'content-type': 'text/html' });
             res.end(err);
           } else {
-            res.writeHead(200, { "content-type": "text/html" });
+            res.writeHead(200, { 'content-type': 'text/html' });
             res.end(data);
           }
         });
       }
     });
   });
-  req.on("error", err => {});
+  req.on('error', err => {});
 };
 
 //login page
 const login = (req, res) => {
-  let data = "";
-  req.on("data", chunk => {
+  let data = '';
+  req.on('data', chunk => {
     data += chunk;
     console.log(chunk);
   });
-  req.on("end", () => {
-
+  req.on('end', () => {
     const submittedLogin = queryString.parse(data);
 
     getData.validUser(submittedLogin.email, (err, result) => {
@@ -88,26 +85,65 @@ const login = (req, res) => {
           result.password,
           (err, hashed) => {
             if (err) {
-              console.log(err, "user does not exist");
+              console.log(err, 'user does not exist');
               // res.writeHead(302, {
               //   "Location": "/login"
               // });
             } else {
-              var user = {
-                'email': submittedLogin.email
+              const user = {
+                email: submittedLogin.email,
+                id: result.id
               };
               const signJWT = sign(user, process.env.SECRET);
               res.setHeader('Set-Cookie', `jwt=${signJWT};  Max-Age:9000`);
               res.writeHead(302, {
-                "Location": "/chatRoom"
+                Location: '/chatRoom'
               });
               res.end();
             }
-          });
+          }
+        );
       }
     });
   });
-  req.on("error", err => {});
+  req.on('error', err => {});
 };
 
-module.exports = { publicHandler, createUser, login };
+//message page
+const message = (req, res) => {
+  let message = '';
+  req.on('data', chunk => {
+    message += chunk;
+  });
+  req.on('end', () => {
+    const newMessage = queryString.parse(message);
+    verify(
+      req.headers.cookie.split('=')[1],
+      process.env.SECRET,
+      (err, decodedCookie) => {
+        if (err) {
+          res.writeHead(401, { 'Content-Type': 'text/html' });
+          res.end('you are using an invalid cookie');
+        } else {
+          const messagePost = {
+            userid: decodedCookie.id,
+            time_stamp: JSON.stringify(new Date()),
+            description: newMessage.description
+          };
+          postData.createMessage(messagePost, (err, createMessageResult) => {
+            if (err) {
+              res.writeHead(500, { 'Content-Type': 'text/html' });
+              res.end('description is not valid');
+            } else {
+              res.writeHead(200, { 'Content-Type': 'text/html' });
+              res.end();
+            }
+          });
+        }
+      }
+    );
+  });
+  req.on('error', err => {});
+};
+
+module.exports = { publicHandler, createUser, login, message };
--- src/router.js
+++ src/router.js
@@ -3,12 +3,19 @@ const qs = require('querystring');
 
 const router = (req, res) => {
   const url = req.url;
-  if (url === '/' || url.includes('/public') || url.includes('/signup') || url.includes('/chatRoom')) {
+  if (
+    url === '/' ||
+    url.includes('/public') ||
+    url.includes('/signup') ||
+    url.includes('/chatRoom')
+  ) {
     handler.publicHandler(req, res);
   } else if (url.includes('/registration')) {
     handler.createUser(req, res);
   } else if (url.includes('/login')) {
     handler.login(req, res);
+  } else if (url.includes('/newMessage')) {
+    handler.message(req, res);
   } else {
     res.writeHead(404, { 'content-type': 'text/html' });
     res.end('error');
